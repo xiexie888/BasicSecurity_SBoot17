dd6a5f42ff9f3a09712e62d881e4512b15903276
GBTC408004UR <GBTC408004UR@MC.MONTGOMERYCOLLEGE.EDU>
2017-11-27 19:39:49 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HomeController
+
+{
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/admin/first")
+    public String admin() {return "admin";}
+}
